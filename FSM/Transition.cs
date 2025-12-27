46049fa69fb5106f121b8fd157dd14c02e855d16
Ahmed GD <104320450+AhmedGD1@users.noreply.github.com>
2025-12-27 20:44:12 +0200

    Update Transition.cs

--- FSM/Transition.cs
+++ FSM/Transition.cs
@@ -5,22 +5,22 @@ namespace Godot.FSM;
 
 public class Transition<T> where T : Enum
 {
-    private static long globalInsertionCounter = 0;
+    private static int globalInsertionCounter = 0;
+    private long InsertionIndex { get; set; }
 
     public T From { get; private set; }
     public T To { get; private set; }
 
     public Predicate<StateMachine<T>> Condition { get; private set; }
     public Predicate<StateMachine<T>> Guard { get; private set; }
+    public Action OnTriggered { get; private set; }
 
     public string EventName { get; private set; }
+
     public float OverrideMinTime { get; private set; } = -1f;
     public int Priority { get; private set; }
-    public long InsertionIndex { get; private set; }
 
     public bool ForceInstantTransition { get; private set; }
-
-    public Action Triggered { get; private set; }
 
     public Transition(T from, T to)
     {
@@ -30,10 +30,9 @@ public class Transition<T> where T : Enum
         InsertionIndex = globalInsertionCounter++;
     }
 
-
-    public Transition<T> OnTriggered(Action callback)
+    public Transition<T> OnTrigger(Action method)
     {
-        Triggered = callback;
+        OnTriggered = method;
         return this;
     }
 
@@ -46,7 +45,7 @@ public class Transition<T> where T : Enum
     public Transition<T> SetCondition(Predicate<StateMachine<T>> condition)
     {
         Condition = condition;
-        return this;   
+        return this;
     }
 
     public Transition<T> SetGuard(Predicate<StateMachine<T>> guard)
@@ -55,9 +54,9 @@ public class Transition<T> where T : Enum
         return this;
     }
 
-    public Transition<T> SetMinTime(float minTime)
+    public Transition<T> SetMinTime(float value)
     {
-        OverrideMinTime = Math.Max(0f, minTime);
+        OverrideMinTime = value;
         return this;
     }
 
@@ -67,7 +66,7 @@ public class Transition<T> where T : Enum
         return this;
     }
 
-    public Transition<T> SetOnTop()
+    public Transition<T> SetMaxPriority()
     {
         Priority = int.MaxValue;
         return this;
@@ -85,10 +84,10 @@ public class Transition<T> where T : Enum
         return this;
     }
 
-    internal static int Compare(Transition<T> a, Transition<T> b)
+    internal static int Compare(Transition<T> transition1, Transition<T> transition2)
     {
-        int priorityCompare = b.Priority.CompareTo(a.Priority);
-        return priorityCompare != 0 ? priorityCompare : a.InsertionIndex.CompareTo(b.InsertionIndex);
+        int priorityCompare = transition2.Priority.CompareTo(transition1.Priority);
+        return priorityCompare != 0 ? priorityCompare : transition1.InsertionIndex.CompareTo(transition2.InsertionIndex);
     }
 }
 
